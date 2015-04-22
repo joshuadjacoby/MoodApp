befd9b8ce6a7b0c8c8125f332e1adc2731f4ec58
Josh <joshuadjacoby@gmail.com>
2015-04-21 23:14:39 -0700

    added clicking buttons and updating image and same for text

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Button : MonoBehaviour {
+    public GameObject mainImage;
+    private int moodNumber;
+    public Sprite[] moods;
+    // Use this for initialization
+	void Start () {
+        moodNumber = int.Parse(this.name.Substring(1));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    public void changeImage() {
+        Debug.Log("IMAGE CHANGED, we are " + moodNumber);
+        mainImage.GetComponent<Image>().sprite = moods[moodNumber];
+    }
+
+    
+}
---
+++ Assets/Scripts/iButton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class iButton : MonoBehaviour {
+    public GameObject mainText;
+    private int moodNumber;
+    private string[] moods;
+    // Use this for initialization
+	void Start () {
+        moodNumber = int.Parse(this.name.Substring(1));
+        moods = new string[5]{"Happy", "Sad", "Angry", "Embarrassed", "Scared"};
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    public void changeImage() {
+        Debug.Log("IMAGE CHANGED, we are " + moodNumber);
+        mainText.GetComponent<Text>().text = moods[moodNumber];
+    }
+
+    
+}
