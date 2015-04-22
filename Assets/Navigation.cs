8dfd7b0f34032092033c1dc9e99ac7fe39b8c551
ArcanePariah <durandel12345@gmail.com>
2015-04-22 12:57:52 -0700

    Main Page + Navigation

---
+++ Assets/Navigation.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Navigation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void playButtonClicked()
+	{
+		Application.LoadLevel ("Play");	
+	}
+
+	public void learnButtonClicked()
+	{
+		Application.LoadLevel ("Learn");
+	}
+
+
+}
